9ce3dfd05d223d5f7aed26aca131edc0eab3de0b
Sruthileela <sruthiyarl@gmail.com>
2026-01-10 20:45:17 +0530

    Update generate_data.py

--- generate_data.py
+++ generate_data.py
@@ -2,28 +2,27 @@ import numpy as np
 import pandas as pd
 
 np.random.seed(42)
+n = 1500
+t = np.arange(n)
 
-n_rows = 1500
-time = np.arange(n_rows)
-
-# Generate seasonal + trend data
 target = (
-    0.05 * time +
-    10 * np.sin(2 * np.pi * time / 24) +
-    5 * np.sin(2 * np.pi * time / 168) +
-    np.random.normal(0, 1, n_rows)
+    0.03 * t +
+    8 * np.sin(2 * np.pi * t / 24) +
+    4 * np.sin(2 * np.pi * t / 168) +
+    np.random.normal(0, 1, n)
 )
 
-feature_1 = target + np.random.normal(0, 0.5, n_rows)
-feature_2 = np.cos(2 * np.pi * time / 24) + np.random.normal(0, 0.2, n_rows)
-feature_3 = np.random.normal(0, 1, n_rows)
+feature_1 = target + np.random.normal(0, 0.3, n)
+feature_2 = np.cos(2 * np.pi * t / 24) + np.random.normal(0, 0.2, n)
+feature_3 = np.sin(2 * np.pi * t / 168) + np.random.normal(0, 0.2, n)
+feature_4 = np.random.normal(0, 1, n)
 
 df = pd.DataFrame({
     "target": target,
     "feature_1": feature_1,
     "feature_2": feature_2,
-    "feature_3": feature_3
+    "feature_3": feature_3,
+    "feature_4": feature_4
 })
 
 df.to_csv("data.csv", index=False)
-print("data.csv generated with", len(df), "rows")
