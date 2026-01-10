90959c2f4d11541fe550e6863939e4f5f81e6660
Sruthileela <sruthiyarl@gmail.com>
2026-01-10 19:27:07 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,44 +1,27 @@
-import tensorflow as tf
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.models import Model
+import torch
+import torch.nn as nn
 
-
-class NBeatsBlock(tf.keras.layers.Layer):
-    def __init__(self, units, theta_dim, backcast_length, forecast_length):
+class TransformerTimeSeries(nn.Module):
+    def __init__(self, input_dim, d_model=64, nhead=4, num_layers=2):
         super().__init__()
-        self.fc1 = Dense(units, activation="relu")
-        self.fc2 = Dense(units, activation="relu")
-        self.theta = Dense(theta_dim)
 
-        self.backcast_length = backcast_length
-        self.forecast_length = forecast_length
+        self.embedding = nn.Linear(input_dim, d_model)
 
-    def call(self, x):
-        x = self.fc1(x)
-        x = self.fc2(x)
-        theta = self.theta(x)
+        encoder_layer = nn.TransformerEncoderLayer(
+            d_model=d_model,
+            nhead=nhead,
+            batch_first=True
+        )
 
-        backcast = theta[:, :self.backcast_length]
-        forecast = theta[:, self.backcast_length:]
+        self.transformer = nn.TransformerEncoder(
+            encoder_layer,
+            num_layers=num_layers
+        )
 
-        return backcast, forecast
+        self.fc = nn.Linear(d_model, 1)
 
-
-def build_nbeats(input_size, units=256, stacks=3):
-    forecast_length = 1
-    backcast_length = input_size
-    theta_dim = backcast_length + forecast_length
-
-    inputs = tf.keras.Input(shape=(input_size,))
-    residual = inputs
-    forecast = tf.zeros_like(inputs[:, :forecast_length])
-
-    for _ in range(stacks):
-        block = NBeatsBlock(units, theta_dim, backcast_length, forecast_length)
-        backcast, block_forecast = block(residual)
-        residual = residual - backcast
-        forecast = forecast + block_forecast
-
-    model = Model(inputs, forecast)
-    model.compile(optimizer="adam", loss="mse")
-    return model
+    def forward(self, x):
+        x = self.embedding(x)
+        x = self.transformer(x)
+        out = self.fc(x[:, -1, :])
+        return out.squeeze()
